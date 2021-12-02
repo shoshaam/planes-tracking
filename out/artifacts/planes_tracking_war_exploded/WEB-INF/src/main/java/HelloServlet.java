863067b50a3b6ebcc4e12a016d1546c5646fa826
shoshaam <alexandramartinovich1809@gmail.com>
2021-12-02 14:47:39 +0300

    hello-word-app

--- out/artifacts/planes_tracking_war_exploded/WEB-INF/src/main/java/HelloServlet.java
+++ out/artifacts/planes_tracking_war_exploded/WEB-INF/src/main/java/HelloServlet.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
