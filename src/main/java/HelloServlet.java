42cc11693b2fa0f2addc54168a37de1caff8bd1f
shoshaam <alexandramartinovich1809@gmail.com>
2021-12-06 20:39:40 +0300

    Git branch hello-world

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,22 @@
+import com.google.gson.JsonObject;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+        PrintWriter out = response.getWriter();
+
+        JsonObject json = new JsonObject();
+        json.addProperty("HelloWorld", "Hello world!");
+        out.print(json);
+        out.flush();
+
+    }
+}
